6b6969c84e5157aeb9d3cc922bb1d2797b6ddf21
CK <cklee7755@gmail.com>
2024-01-11 17:31:28 +0800

    Files and changes requires for ConfigMap

--- corona-tracker-backend/src/main/java/com/hackforfun/coronatrackerbackend/CoronaTrackerBackendApplication.java
+++ corona-tracker-backend/src/main/java/com/hackforfun/coronatrackerbackend/CoronaTrackerBackendApplication.java
@@ -2,10 +2,14 @@ package com.hackforfun.coronatrackerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoRepositories
 public class CoronaTrackerBackendApplication {
 
 	public static void main(String[] args) {
